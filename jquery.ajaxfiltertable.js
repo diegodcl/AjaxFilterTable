c57be926aa2246bf26912ecadf578fde7ea3899c
diegodcl <diegodcl@hotmail.com>
2016-08-02 17:25:05 -0300

    Add files via upload

--- jquery.ajaxfiltertable.js
+++ jquery.ajaxfiltertable.js
@@ -1,11 +1,11 @@
 (function( $ ) {
 	
-	var records={};
+	var records= {};
 	var columnMap = [];
 	var totalQuery = 0;
 	var totalRec = 0;
 	var totalPages = 0;
-	var page = 0;
+	var page = 1;
 	var currPage = 1;
 	var table;
 
@@ -17,7 +17,7 @@
 	
 	function getData(url,filters,perPage,page, handleData) {
 		
-		// data do .ajax só aceita uma string ou um array, pesquisar uma soluçõa mais elegante pra enviar pro controller
+		// data do .ajax só aceita uma string ou um array, pesquisar uma solução mais elegante pra enviar pro controller
 		filters["perPage"] = perPage;
 		filters["page"] = page;
 		
@@ -32,8 +32,8 @@
 		});
 	}
 	
-	function mapTable(element) {
-		tableHead = $(element).find("tr").first().find("th");
+	function mapTable() {
+		tableHead = $(table).find("tr").first().find("th");
 
 		$.each(tableHead, function() {
 			index = $(this).index();
@@ -53,9 +53,9 @@
 		$(element).append(line);
 	}
 	
-	function fillTable(element) {
+	function fillTable() {
 		var lineMap = []
-		tablebody = $(element).children("tbody");
+		tablebody = $(table).children("tbody");
 		var newLine;
 
 		$(tablebody).empty();
@@ -69,46 +69,34 @@
 		});
 	}
 
-	function createFoot(element)
+	function createFoot()
 	{
-		if ($(element).find("tfoot").length < 1)
-			$(element).append("<tfoot></tfoot>");
+		var tofoot = "";
 		
-		footer = $(element).find("tfoot");
+		if ($(table).find("tfoot").length < 1)
+			$(table).append("<tfoot></tfoot>");
 		
-		if ($(footer).find("th").find("#pagination").length < 1)
-			$(footer).append("<tr><th colspan=\""+$(element).find("thead").find("th").length+"\"><div id=\"pagination\"></div></th></tr>");
+		footer = $(table).find("tfoot");
 		
-		footerCel = $(footer).find("th").find("#pagination");
+		$(footer).find("tr").remove();
 		
-		if ($(footerCel).find("#anterior").length < 1)
-			$(footerCel).append("<div id=\"anterior\"><a data-page=\""+(page-1)+"\" >Anterior</a></div>");
-		
-		$(footerCel).find("#pagesLinks").remove();
-		
-		pagesLinks = "<div id=\"pagesLinks\"><ul>";
-		
-		console.log("totalPages "+totalPages);
-		
+		tofoot= "<tr><td colspan=\""+$(table).find("thead").find("th").length+"\">";
+		tofoot+= "<ul><li><a data-page=\""+(page-1)+"\" >Anterior</a></li>";
+
 		for(i=1;i<=totalPages;i++)
 		{
-			pagesLinks+= "<li><a data-page=\""+i+"\" >"+i+"</a></li>";
+			tofoot+= "<li><a data-page=\""+i+"\" >"+i+"</a></li>";
 		}
 		
-		if ($(footerCel).find("#pagesLinks").length < 1)
-			pagesLinks+= "</ul></div>";
-		
-		$(footerCel).append(pagesLinks);
-		
-		if ($(footerCel).find("#proxima").length < 1)
-			$(footerCel).append("<div id=\"proxima\"><a data-page=\""+(page+1)+"\" >Próxima</a></div>");
-		
-		$(footerCel).find("#pagesLinks").find("a").on("click", changePage);
+		tofoot+= "<li><a data-page=\""+(page+1)+"\" >Próxima</a></li></ul></td></tr>";
+
+		$(footer).append(tofoot);
+		$(footer).find("td").find("ul>li").find("a").on("click", changePage);
 	}
 	
-	function loadTable(obj,page) {
+	function loadTable(page) {
 		
-		$(obj).find("tbody").find("tr").remove();
+		$(table).find("tbody").find("tr").remove();
 		
 		getData(settings.url, settings.filters, settings.perPage, page, function(output) {
 			records = output;
@@ -116,18 +104,17 @@
 		
 		totalQuery = records["queryRecordCount"];
 		totalRec = records["totalRecordCount"];
-		console.log("totalRec "+totalRec+" / totalQuery "+totalQuery+" = "+(totalRec / settings.perPage));
 		totalPages = Math.ceil(totalRec / settings.perPage);
 		
-		mapTable(obj);
-		fillTable(obj);
-		createFoot(obj);
+		mapTable();
+		fillTable();
+		createFoot();
 	}
 	
 	function changePage()
 	{
 		var page = $(this).attr('data-page');
-		loadTable(table,page);
+		loadTable(page);
 	}
 	
 	$.fn.AjaxFilterTable = function( options ) {
@@ -143,7 +130,8 @@
 		return this.each( function() {
 			
 			table = this;
-			loadTable(table);
+			loadTable();
+			$(table).addClass("ajaxFilterTable");
 			
         });
 	}
